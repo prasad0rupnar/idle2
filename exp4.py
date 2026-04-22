56d025816da0eafe8a69e5f48c5869fbabd68533
prasad0rupnar <amar0mark2005@gmail.com>
2026-04-23 00:14:57 +0530

    Update exp4.py

--- exp4.py
+++ exp4.py
@@ -1,20 +1,29 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+from sklearn import datasets
 from sklearn.linear_model import LogisticRegression
-data = pd.read_csv("voting_data.csv")
-X = data[['Campaign_Spending', 'Approval_Rating']]
-y = data['Result']
-model = LogisticRegression()
-model.fit(X, y)
-predictions = model.predict(X)
-plt.scatter(
-data['Campaign_Spending'],
-data['Approval_Rating'],
-c=y,
-cmap='bwr',
-edgecolors='k'
+import matplotlib.pyplot as plt
+import numpy as np
+
+d = datasets.load_iris()
+X = d.data[:, :2]
+y = d.target
+
+m = LogisticRegression(max_iter=200)
+m.fit(X, y)
+
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+xx, yy = np.meshgrid(
+    np.linspace(x_min, x_max, 100),
+    np.linspace(y_min, y_max, 100)
 )
-plt.xlabel("Campaign Spending (Millions)")
-plt.ylabel("Approval Rating (%)")
-plt.title("Election Outcome Prediction (Logistic Regression)")
+
+Z = m.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = Z.reshape(xx.shape)
+
+plt.contourf(xx, yy, Z, alpha=0.3)
+plt.scatter(X[:, 0], X[:, 1], c=y, edgecolor='k')
+plt.xlabel("Feature 1")
+plt.ylabel("Feature 2")
+plt.title("Logistic Regression Decision Boundary")
 plt.show()
