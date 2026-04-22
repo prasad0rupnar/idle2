723ad118f8565796ed7630416a4175c82e563eee
prasad0rupnar <amar0mark2005@gmail.com>
2026-04-23 01:21:09 +0530

    Update exp4.py

--- exp4.py
+++ exp4.py
@@ -1,29 +1,16 @@
 from sklearn import datasets
 from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
-import numpy as np
 
+# load data
 d = datasets.load_iris()
-X = d.data[:, :2]
+X = d.data[:,:2]   # first 2 features
 y = d.target
 
-m = LogisticRegression(max_iter=200)
-m.fit(X, y)
+# model
+m = LogisticRegression()
+m.fit(X,y)
 
-x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-
-xx, yy = np.meshgrid(
-    np.linspace(x_min, x_max, 100),
-    np.linspace(y_min, y_max, 100)
-)
-
-Z = m.predict(np.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
-
-plt.contourf(xx, yy, Z, alpha=0.3)
-plt.scatter(X[:, 0], X[:, 1], c=y, edgecolor='k')
-plt.xlabel("Feature 1")
-plt.ylabel("Feature 2")
-plt.title("Logistic Regression Decision Boundary")
+# plot
+plt.scatter(X[:,0], X[:,1], c=y)
 plt.show()
