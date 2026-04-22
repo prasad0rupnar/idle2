a0982d5ed2dfc104f500b1e376d14d6c74bced76
prasad0rupnar <amar0mark2005@gmail.com>
2026-04-23 00:16:58 +0530

    Update exp6.py

--- exp6.py
+++ exp6.py
@@ -1,57 +1,22 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-# Hebbian Network Class
-class HebbianNetwork:
-    def __init__(self, input_size, output_size, learning_rate=0.1):
-        self.weights = np.random.rand(input_size, output_size)
-        self.learning_rate = learning_rate
-    
-    def train(self, inputs, outputs):
-        for i in range(len(inputs)):
-            input_vector = inputs[i]
-            output_vector = outputs[i]
-            # Hebbian weight update rule
-            self.weights += self.learning_rate * np.outer(input_vector, output_vector)
-    
-    def predict(self, input_vector):
-        return np.dot(input_vector, self.weights)
+# training data
+X = np.array([[1,1],[1,-1],[-1,1],[-1,-1]])
+y = np.array([1,-1,-1,1])
 
-# Training data
-inputs = np.array([
-    [1, 0],
-    [0, 1],
-    [1, 1],
-    [0, 0]
-])
+w = np.zeros(2)
 
-outputs = np.array([
-    [1, 0],
-    [0, 1],
-    [1, 1],
-    [0, 0]
-])
+for i in range(len(X)):
+    w = w + X[i] * y[i]
 
-# Create network
-network = HebbianNetwork(input_size=2, output_size=2)
+print("Final Weights:", w)
 
-# Train network
-network.train(inputs, outputs)
+sns.scatterplot(x=X[:,0], y=X[:,1], hue=y, palette='coolwarm', s=100)
 
-# Test prediction
-test_input = np.array([1, 0])
-predicted_output = network.predict(test_input)
-print("Predicted output for input", test_input, ":", predicted_output)
-
-# Plot weight matrix heatmap
-hebbian_weights = network.weights
-
-plt.figure(figsize=(6,5))
-sns.heatmap(hebbian_weights, annot=True, cmap="coolwarm", fmt=".3f", 
-            xticklabels=["Output Neuron 1", "Output Neuron 2"], 
-            yticklabels=["Input Neuron 1", "Input Neuron 2"])
-plt.title("Hebbian Learning Weight Matrix")
-plt.xlabel("Output Neurons")
-plt.ylabel("Input Neurons")
+plt.title("Hebbian Learning")
+plt.xlabel("X1")
+plt.ylabel("X2")
+plt.grid(True)
 plt.show()
